001a6be2c3b6bfc984edbc3319d6d794f0a2b0b3
igorgll <igorlimagn@gmail.com>
2023-09-28 20:28:12 -0300

    Criando classe servlet para requisições http

---
+++ src/main/java/br/com/perritoCaliente/servlet/CreateAccount.java
@@ -0,0 +1,24 @@
+package br.com.perritoCaliente.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/create-account")
+public class CreateAccount extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String name = request.getParameter("name");
+//        String user;
+//        String email;
+//        String password;
+
+        System.out.println(name);
+
+        request.getRequestDispatcher("index.jsp").forward(request, response);
+    }
+}
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,5 +1,13 @@
 <html>
 <body>
 <h2>Hello World!</h2>
+<form action="/create-account" method="post">
+
+    <label>Seu nome</label>
+    <input type="text" name="name" id="=name">
+
+    <button type="submit">Register</button>
+
+</form>
 </body>
 </html>
